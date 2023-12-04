7da9ee83e4d9bc067e72b77025b07abb4f40974e
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2023-12-04 22:57:11 +0530

    reverse

---
+++ self_javascript_pratice/01_reverse.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,3,4,5];
+let newArr = arr.reverse()
+console.log(newArr)
+
+let uniqueArray = []
+
+let string = "hello world";
+let newString = string.split('').reverse().join('')
+console.log(newString)
